56d86cde1cdc36d828234d09b358fabc760f5d00
Qurbonov-Rustambek <qurbonovrustamjon007@gmail.com>
2022-05-14 10:35:34 +0500

    add js royekt

---
+++ app.js
@@ -0,0 +1,9 @@
+let ism = prompt("ismingizni kiriting");
+
+let resuat = ism.charAt();
+let resualt1 = resuat.toUpperCase();
+let resualt2 = ism.slice(1);
+let resualt3 = resualt2.toLowerCase();
+let resualt4 = resualt1 + resualt3;
+
+console.log(resualt4);
